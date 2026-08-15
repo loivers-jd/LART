3375b06ea29c629bc405df404f8fc341cc674398
Louise <louisedarantinao13@gmail.com>
2026-08-15 14:10:00 +0800

    Add files via upload

---
+++ lartparty/LartParty.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lartparty;
+
+/**
+ *
+ * @author CompLabC209-PC13
+ */
+public class LartParty {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
